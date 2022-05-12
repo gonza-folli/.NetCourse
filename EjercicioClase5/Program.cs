6f8d335d7a80417ccc0a5feb09840a4b78a6a085
gonza-folli <gonza_folli@hotmail.com>
2022-05-12 11:44:08 -0300

    Ejercicio Clase 5

---
+++ EjercicioClase5/Program.cs
@@ -0,0 +1,21 @@
+﻿var respuesta = "S";
+
+while (respuesta?.ToUpper() == "S")
+{
+    Console.WriteLine("Ingrese un nombre");
+    var nombre = Console.ReadLine();
+
+    Console.WriteLine($"Hola {nombre}!!!");
+
+    Console.WriteLine("Desea continuar? S/N");
+    respuesta = Console.ReadLine();
+
+}
+if (respuesta?.ToUpper() == "N")
+{
+    Console.WriteLine("Programa finalizado correctamente");
+}
+else
+{
+    Console.WriteLine("Opcion no valida");
+}
