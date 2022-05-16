df42907e6763242923274924db0071c1aec4abd3
gonza-folli <gonza_folli@hotmail.com>
2022-05-16 11:38:16 -0300

    Clase6

---
+++ EjercicioClase6/Program.cs
@@ -0,0 +1,23 @@
+﻿int numeroSecreto = new Random(DateTime.Now.Millisecond).Next(1, 21);
+int intentos = 0;
+int numeroElegido = -1;
+
+while(numeroSecreto != numeroElegido)
+{
+    Console.WriteLine("Ingrese el número que piense es el secreto");
+    numeroElegido = int.Parse(Console.ReadLine());
+    if (numeroElegido > numeroSecreto)
+    {
+        Console.WriteLine("el número ingresado es MAYOR al secreto");
+        intentos++;
+    } else if (numeroElegido < numeroSecreto)
+    {
+        Console.WriteLine("el número ingresado es MENOR al secreto");
+        intentos++;
+    }
+}
+Console.WriteLine($"Felicitaciones, has adivinado el número secreto que era: {numeroSecreto}");
+Console.WriteLine($"Lo has logrado en {intentos+1} intentos!!");
+
+Console.Read();
+
