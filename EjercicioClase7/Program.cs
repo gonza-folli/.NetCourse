2b00f8ae21a5c1082aaa4b7d78e5054fb45fbd80
gonza-folli <gonza_folli@hotmail.com>
2022-05-23 09:49:09 -0300

    Ejercicio Clase 7

---
+++ EjercicioClase7/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] misNumeros = new int[10];
+
+for (int i = 0; i < 10; i++)
+{
+	Console.WriteLine($"Ingrese el {i + 1}º Número");
+	misNumeros[i] = int.Parse(Console.ReadLine());
+}
+
+int suma = 0;
+int max = misNumeros[0];
+int min = misNumeros[0];
+
+foreach (int num in misNumeros)
+{
+	suma += num;
+	if (num > max) max = num;
+	if (num < min) min = num;
+}
+
+Console.WriteLine($"La suma de los numeros es {suma}");
+Console.WriteLine($"El Mayor es {max}");
+Console.WriteLine($"El Menor es {min}");
+Console.WriteLine($"El promedio de los numeros es {suma / misNumeros.Length}");
+
+Console.WriteLine($"Los números ingresados son:");
+
+foreach (int num in misNumeros)
+{
+	Console.WriteLine(num);
+}
+
+Console.Read();
