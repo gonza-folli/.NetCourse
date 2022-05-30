eeea546fb288fbedd0ee0a10cda76e0432b2a1cf
gonza-folli <gonza_folli@hotmail.com>
2022-05-30 16:02:05 -0300

    Ejercicio 1 Clase 9

---
+++ EjercicioClase9-1/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Ingrese el total de elementos del vector");
+int cantidad = int.Parse(Console.ReadLine());
+
+int[] vector = new int[cantidad];
+Random aleatorio = new Random();
+
+for (int i = 0; i < vector.Length; i++)
+{
+    vector[i] = aleatorio.Next(100);
+}
+
+Console.WriteLine("Los elementos del vector son: ");
+
+foreach(int i in vector)
+{
+    Console.WriteLine(i);
+}
+
+for (int i = 0; i < vector.Length /2 ; i++)
+{
+    int aux = vector[i];
+    vector[i] = vector[vector.Length-i-1];
+    vector[vector.Length-i-1] = aux;
+}
+
+Console.WriteLine("Los elementos INVERTIDOS del vector son: ");
+foreach (int i in vector)
+{
+    Console.WriteLine(i);
+}
+
+Console.Read();
